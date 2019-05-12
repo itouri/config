87dfe304dbeb27ecdbbe319a8584e9748093884c
itouri <hechima.itouri@gmail.com>
2019-05-12 11:39:03 +0900

    modified

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # https://github.com/f96q/dotfiles/blob/master/setup.sh
 
-for name in tmux.conf vimrc
+for name in tmux.conf vimrc bashrc
 do
     rm -rf $HOME/.$name
     ln -s `pwd`/$name $HOME/.$name
