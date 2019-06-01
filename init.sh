077e00f72630af975442ab72071dc351be9c6194
itouri <hechima.itouri@gmail.com>
2019-06-01 20:34:25 +0900

    f

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo apt -y update
 sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible
+ansible-playbook -i localhost, -c local playbook.yml
