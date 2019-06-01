91a4d5c3c6c86c68d2b47a1ef61ee6c62a1523fa
itouri <hechima.itouri@gmail.com>
2019-06-01 21:21:06 +0900

    f

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo apt -y update
 sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo add-apt-repository --yes --update ppa:longsleep/golang-backports
 sudo apt -y install ansible
 ansible-playbook -i localhost, -c local playbook.yml
