4dbc9135a1164a87e30be1c1ed3e65a7095714a7
itouri <hechima.itouri@gmail.com>
2019-06-01 20:29:52 +0900

    dirty

--- init.sh
+++ init.sh
@@ -2,5 +2,3 @@ sudo apt -y update
 sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible
-ansible-playbook -i localhost, -c local playbook.yml
-
---
+++ init2.sh
@@ -0,0 +1,2 @@
+ansible-playbook -i localhost, -c local playbook2.yml
+source ~/.bashrc
