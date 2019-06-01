ba483d74afcac108dac956e1692d4cf3aa04ce37
itouri <hechima.itouri@gmail.com>
2019-06-01 22:00:48 +0900

    f

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo add-apt-repository --yes --update ppa:longsleep/golang-backports
 sudo apt -y install ansible
 ansible-playbook -i localhost, -c local playbook.yml
+fish -c "fisher add jethrokuan/z" && fish -c "fisher add decors/fish-ghq" && fish -c "fisher add oh-my-fish/theme-bobthefish"
